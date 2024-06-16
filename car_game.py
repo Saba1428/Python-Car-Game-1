f2823d88e2b5f2e9ab978dc8122c7ca096d32adf
Saba1428 <sabuka200912@gmail.com>
2024-06-16 12:28:21 +0400

    Add files via upload

--- car_game.py
+++ car_game.py
@@ -4,48 +4,48 @@ import random
 
 pygame.init()
 
-# create the window
+# ფანჯრის შექმნა
 width = 500
 height = 500
 screen_size = (width, height)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Car Game')
 
-# colors
+# ფერები
 gray = (100, 100, 100)
 green = (76, 208, 56)
 red = (200, 0, 0)
 white = (255, 255, 255)
 yellow = (255, 232, 0)
 
-# road and marker sizes
+# გზის და მარკერის ზომები
 road_width = 300
 marker_width = 10
 marker_height = 50
 
-# lane coordinates
+# ზოლის კოორდინატები
 left_lane = 150
 center_lane = 250
 right_lane = 350
 lanes = [left_lane, center_lane, right_lane]
 
-# road and edge markers
+# გზის და კიდეების მარკერები
 road = (100, 0, road_width, height)
 left_edge_marker = (95, 0, marker_width, height)
 right_edge_marker = (395, 0, marker_width, height)
 
-# for animating movement of the lane markers
+# ზოლის მარკერების მოძრაობის ანიმაციისთვის
 lane_marker_move_y = 0
 
-# player's starting coordinates
+# მოთამაშის საწყისი კოორდინატები
 player_x = 250
 player_y = 400
 
-# frame settings
+# ჩარჩოს პარამეტრები
 clock = pygame.time.Clock()
 fps = 120
 
-# game settings
+# თამაშის პარამეტრები
 gameover = False
 speed = 2
 score = 0
@@ -55,7 +55,7 @@ class Vehicle(pygame.sprite.Sprite):
     def __init__(self, image, x, y):
         pygame.sprite.Sprite.__init__(self)
         
-        # scale the image down so it's not wider than the lane
+        # შეამცირეთ სურათი ისე, რომ არ იყოს უფრო ფართო ვიდრე ზოლი
         image_scale = 45 / image.get_rect().width
         new_width = image.get_rect().width * image_scale
         new_height = image.get_rect().height * image_scale
@@ -70,26 +70,26 @@ class PlayerVehicle(Vehicle):
         image = pygame.image.load('car.png')
         super().__init__(image, x, y)
         
-# sprite groups
+# sprite ჯგუფები
 player_group = pygame.sprite.Group()
 vehicle_group = pygame.sprite.Group()
 
-# create the player's car
+# შექმენით მოთამაშის მანქანა
 player = PlayerVehicle(player_x, player_y)
 player_group.add(player)
 
-# load the vehicle images
+# ჩატვირთეთ მანქანის სურათები
 image_filenames = ['pickup_truck.png', 'semi_trailer.png', 'taxi.png', 'van.png']
 vehicle_images = []
 for image_filename in image_filenames:
     image = pygame.image.load(image_filename)
     vehicle_images.append(image)
     
-# load the crash image
+# ჩატვირთეთ ავარიის სურათი
 crash = pygame.image.load('crash.png')
 crash_rect = crash.get_rect()
 
-# game loop
+# თამაშის მარყუჟი
 running = True
 while running:
     
@@ -99,7 +99,7 @@ while running:
         if event.type == QUIT:
             running = False
             
-        # move the player's car using the left/right arrow keys
+        # გადაიტანეთ მოთამაშის მანქანა მარცხენა და მარჯვენა ისრიანი ღილაკების გამოყენებით
         if event.type == KEYDOWN:
             
             if event.key == K_LEFT and player.rect.center[0] > left_lane:
@@ -107,14 +107,14 @@ while running:
             elif event.key == K_RIGHT and player.rect.center[0] < right_lane:
                 player.rect.x += 100
                 
-            # check if there's a side swipe collision after changing lanes
+            # შეამოწმეთ არის თუ არა გვერდითი დარტყმის შეჯახება ზოლის შეცვლის შემდეგ
             for vehicle in vehicle_group:
                 if pygame.sprite.collide_rect(player, vehicle):
                     
                     gameover = True
                     
-                    # place the player's car next to other vehicle
-                    # and determine where to position the crash image
+                    # მოათავსეთ მოთამაშის მანქანა სხვა მანქანის გვერდით
+                    # და განსაზღვრეთ სად უნდა განთავსდეს ავარიის სურათი
                     if event.key == K_LEFT:
                         player.rect.left = vehicle.rect.right
                         crash_rect.center = [player.rect.left, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
@@ -123,17 +123,17 @@ while running:
                         crash_rect.center = [player.rect.right, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
             
             
-    # draw the grass
+    # დახატე ბალახი
     screen.fill(green)
     
-    # draw the road
+    # დახაზეთ გზა
     pygame.draw.rect(screen, gray, road)
     
-    # draw the edge markers
+    # დახაზეთ კიდეების მარკერები
     pygame.draw.rect(screen, yellow, left_edge_marker)
     pygame.draw.rect(screen, yellow, right_edge_marker)
     
-    # draw the lane markers
+    # დახაზეთ ზოლის მარკერები
     lane_marker_move_y += speed * 2
     if lane_marker_move_y >= marker_height * 2:
         lane_marker_move_y = 0
@@ -141,13 +141,13 @@ while running:
         pygame.draw.rect(screen, white, (left_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
         pygame.draw.rect(screen, white, (center_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
         
-    # draw the player's car
+    # დახატეთ მოთამაშის მანქანა
     player_group.draw(screen)
     
     # add a vehicle
     if len(vehicle_group) < 2:
         
-        # ensure there's enough gap between vehicles
+        # უზრუნველყოს საკმარისი უფსკრული მანქანებს შორის
         add_vehicle = True
         for vehicle in vehicle_group:
             if vehicle.rect.top < vehicle.rect.height * 1.5:
@@ -155,45 +155,45 @@ while running:
                 
         if add_vehicle:
             
-            # select a random lane
+            # აირჩიეთ შემთხვევითი ზოლი
             lane = random.choice(lanes)
             
-            # select a random vehicle image
+            # აირჩიეთ მანქანის შემთხვევითი სურათი
             image = random.choice(vehicle_images)
             vehicle = Vehicle(image, lane, height / -2)
             vehicle_group.add(vehicle)
     
-    # make the vehicles move
+    # მანქანების მოძრაობა
     for vehicle in vehicle_group:
         vehicle.rect.y += speed
         
-        # remove vehicle once it goes off screen
+        # ამოიღეთ მანქანა ეკრანიდან გასვლის შემდეგ
         if vehicle.rect.top >= height:
             vehicle.kill()
             
-            # add to score
+            # ქულის დამატება
             score += 1
             
-            # speed up the game after passing 5 vehicles
+            # დააჩქარეთ თამაში 5 მანქანის გავლის შემდეგ
             if score > 0 and score % 5 == 0:
                 speed += 1
     
-    # draw the vehicles
+    # დახაზეთ მანქანები
     vehicle_group.draw(screen)
     
-    # display the score
+    # ქულების ჩვენება
     font = pygame.font.Font(pygame.font.get_default_font(), 16)
     text = font.render('Score: ' + str(score), True, white)
     text_rect = text.get_rect()
     text_rect.center = (50, 400)
     screen.blit(text, text_rect)
     
-    # check if there's a head on collision
+    # შეამოწმეთ არის თუ არა თავი შეჯახებისას
     if pygame.sprite.spritecollide(player, vehicle_group, True):
         gameover = True
         crash_rect.center = [player.rect.center[0], player.rect.top]
             
-    # display game over
+    # თამაშის ჩვენება დასრულდა
     if gameover:
         screen.blit(crash, crash_rect)
         
@@ -207,7 +207,7 @@ while running:
             
     pygame.display.update()
 
-    # wait for user's input to play again or exit
+    # დაელოდეთ მომხმარებლის შეყვანის ხელახლა დაკვრას ან გასვლას
     while gameover:
         
         clock.tick(fps)
@@ -218,17 +218,17 @@ while running:
                 gameover = False
                 running = False
                 
-            # get the user's input (y or n)
+            # მიიღეთ მომხმარებლის შეყვანა (y ან n)
             if event.type == KEYDOWN:
                 if event.key == K_y:
-                    # reset the game
+                    # თამაშის გადატვირთვა
                     gameover = False
                     speed = 2
                     score = 0
                     vehicle_group.empty()
                     player.rect.center = [player_x, player_y]
                 elif event.key == K_n:
-                    # exit the loops
+                    # მარყუჟებიდან გასვლა
                     gameover = False
                     running = False
 
